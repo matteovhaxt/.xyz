49f7d171e9f7b2d048f135282a705c6850699015
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-07-11 13:59:59 +0200

    feat: vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,6 @@
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
 injectSpeedInsights();
+inject({ mode: dev ? 'development' : 'production' });
